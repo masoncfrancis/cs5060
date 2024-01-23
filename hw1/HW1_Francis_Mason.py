86ad6052e0295224181109a6a07b49fb4a3ed2a1
Mason <masoncfrancis@gmail.com>
2024-01-22 21:19:38 -0700

    Implemented part of problem 1 in hw 1

--- hw1/HW1_Francis_Mason.py
+++ hw1/HW1_Francis_Mason.py
@@ -1,8 +1,43 @@
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
+
+# The code in this function is a modified version of the code provided in the class example
+def findStop(numbers: list, outputFileName: str, learnOptimal: bool):
+    len_candidates = 100
+    solution_found_count = {}
+    optimal_solution_found_count = {}
+    for i in range(1, len_candidates):
+        solution_found_count[str(i)] = 0
+        optimal_solution_found_count[str(i)] = 0
+
+    for experiment in range(0, 999):
+        candidates = random.sample(numbers, len_candidates)
+        optimal_candidate = max(candidates)
+
+        for i in range(1, len_candidates):
+            for candidate in candidates[i:-1]:
+                if candidate > max(candidates[0:i]):
+                    solution_found_count[str(i)] += 1
+                    if candidate == optimal_candidate:
+                        optimal_solution_found_count[str(i)] += 1
+                    
+                    break
+
+    plt.figure(figsize=(30, 6))  # Adjust the size as needed
+
+    x, y = zip(*optimal_solution_found_count.items())
+
+    plt.plot(x,y)
+    # plt.show()
+    plt.savefig(outputFileName, bbox_inches='tight')
+    
 
 def problem1():
-    fileContents = pd.read_csv('scenario2.csv')
-    print(fileContents)
+    print("Executing code for scenario1.csv")
+    s1FileContents = pd.read_csv('scenario1.csv').iloc[:,0].astype(float).tolist()
+    findStop(s1FileContents, 'scenario1.png', False)
+    
 
 if __name__ == "__main__":
     print("Executing algorithm for problem 1\n")
