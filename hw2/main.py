411e2eafa50e1d91d842b373df512f1587f2f399
Mason <masoncfrancis@gmail.com>
2024-02-03 16:15:24 -0700

    Fixed issue where we couldn't view the plot

--- hw2/main.py
+++ hw2/main.py
@@ -2,39 +2,27 @@
 # Mason Francis
 # CS 5060
 
+import scipy
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-# code from Dr Mario
-def get_probabilities(drift=0):
-    probs = [
-        np.random.normal(0, 5),
-        np.random.normal(-0.5,12),
-        np.random.normal(2,3.9),
-        np.random.normal(-0.5,7),
-        np.random.normal(-1.2,8),
-        np.random.normal(-3,7),
-        np.random.normal(-10,20),
-        np.random.normal(-0.5,1),
-        np.random.normal(-1,2),
-        np.random.normal(1,6),
-        np.random.normal(0.7,4),
-        np.random.normal(-6,11),
-        np.random.normal(-7,1),
-        np.random.normal(-0.5,2),
-        np.random.normal(-6.5,1),
-        np.random.normal(-3,6),
-        np.random.normal(0,8),
-        np.random.normal(2,3.9),
-        np.random.normal(-9,12),
-        np.random.normal(-1,6),
-        np.random.normal(-4.5,8)              
-    ]
-    
-    return probs
+def beta(a, b):
+    beta_dist = scipy.stats.beta(a, b)
+    return beta_dist
 
-def main():
-    # get the probabilities
-    pass
+x = np.linspace(0,1,100)
 
-if __name__ == "__main__":
-    main()
+print(np.random.normal(0.5,0.5))
+a_current = 1
+b_current = 1
+
+for i in range(5):
+    draw = np.random.normal(0.5,0.5)
+    if draw > 0.5:
+        b_current += 1
+    else:
+        a_current += 1
+    plt.plot(beta(a_current, b_current).pdf(x))
+    plt.show()
