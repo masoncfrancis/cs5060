f8f73eb48e7a8856be07636f93b43a6a3d41362c
Mason <masoncfrancis@gmail.com>
2024-02-05 23:08:09 -0700

    implemented part 2

--- hw2/main.py
+++ hw2/main.py
@@ -18,30 +18,35 @@ import scipy
 # My implementation was inspired by material available at https://www.codingninjas.com/studio/library/epsilon-greedy-algorithm
 
 # Dr. Mario's probablilities code
-def get_probabilities(drift=0):
+def get_probabilities(drift=0, step=0):
+
+    # set up probability changes for time step 3000
+    adj0, adj2, adj7, adj18 = 0, 0, 0, 0
+    if step >= 3000:
+        adj0, adj2, adj7, adj18 = 7, 3, 1, 2
     
     probs = [
-        np.random.normal(0, 5),
-        np.random.normal(-0.5,12),
-        np.random.normal(2,3.9),
-        np.random.normal(-0.5,7),
-        np.random.normal(-1.2,8),
-        np.random.normal(-3,7),
-        np.random.normal(-10,20),
-        np.random.normal(-0.5,1),
-        np.random.normal(-1,2),
-        np.random.normal(1,6),
-        np.random.normal(0.7,4),
-        np.random.normal(-6,11),
-        np.random.normal(-7,1),
-        np.random.normal(-0.5,2),
-        np.random.normal(-6.5,1),
-        np.random.normal(-3,6),
-        np.random.normal(0,8),
-        np.random.normal(2,3.9),
-        np.random.normal(-9,12),
-        np.random.normal(-1,6),
-        np.random.normal(-4.5,8)              
+        np.random.normal(0 - drift + adj0,  5),
+        np.random.normal(-0.5 - drift, 12),
+        np.random.normal(2 - drift + adj2, 3.9),
+        np.random.normal(-0.5 - drift, 7),
+        np.random.normal(-1.2 - drift, 8),
+        np.random.normal(-3 - drift, 7),
+        np.random.normal(-10 - drift, 20),
+        np.random.normal(-0.5 - drift + adj7, 1),
+        np.random.normal(-1 - drift, 2),
+        np.random.normal(1 - drift, 6),
+        np.random.normal(0.7 - drift, 4),
+        np.random.normal(-6 - drift, 11),
+        np.random.normal(-7 - drift, 1),
+        np.random.normal(-0.5 - drift, 2),
+        np.random.normal(-6.5 - drift, 1),
+        np.random.normal(-3 - drift, 6),
+        np.random.normal(0 - drift, 8),
+        np.random.normal(2 - drift, 3.9),
+        np.random.normal(-9 - drift + adj18, 12),
+        np.random.normal(-1 - drift, 6),
+        np.random.normal(-4.5 - drift, 8)              
     ]
     
     return probs
@@ -69,7 +74,7 @@ def calculateAverageRewards(rewardsList):
 
     
 # Runs the epsilon greedy algorithm for a given epsilon value    
-def epsilonGreedy(epsilonValue, plot):
+def epsilonGreedy(epsilonValue, plot, movingBandits=False):
     maxRewards = createEmptyList(len(get_probabilities()))
     
     x = range(10000)
@@ -80,6 +85,12 @@ def epsilonGreedy(epsilonValue, plot):
     for i in x:
         
         n = np.random.random() # generate random number to compare with epsilon
+
+        if movingBandits:
+            drift = -0.001 * i
+            probs = get_probabilities(drift=drift, step=i)
+        else: # normal run 
+            probs = get_probabilities()
 
         probs = get_probabilities()
         machineIndex = -1
@@ -102,7 +113,7 @@ def epsilonGreedy(epsilonValue, plot):
     plot.plot(x, y, label=plotLabel)
 
 
-def thompsonSampling(plot):
+def thompsonSampling(plot, movingBandits=False):
     x = range(10000)
     y = []
     rewards = []
@@ -120,8 +131,13 @@ def thompsonSampling(plot):
         
         maxSampleIndex = samples.index(max(samples))
 
+        if movingBandits:
+            drift = -0.001 * i
+            probs = get_probabilities(drift=drift, step=i)
+        else: # normal run 
+            probs = get_probabilities()
 
-        chosenArmVal = get_probabilities()[maxSampleIndex]
+        chosenArmVal = probs[maxSampleIndex]
         rewards.append(chosenArmVal)
         y.append(calculateAverageRewards(rewardsList=rewards))
         
@@ -172,4 +188,28 @@ if __name__ == "__main__":
     # Part 2
     print("Executing part 2 of the assignment")
 
-    # Unfortunately I ran out of time and didn't get to this part :(
+    # Prepare for plotting
+    fig, axs = plt.subplots(1, 2)
+
+
+    # Run Epsilon Greedy algorithms
+    epsilonGreedy(0.01, axs[0], movingBandits=True)
+    epsilonGreedy(0.05, axs[0], movingBandits=True)
+    epsilonGreedy(0.1, axs[0], movingBandits=True)
+    epsilonGreedy(0.4, axs[0], movingBandits=True)
+
+    # plot the reuslts
+    axs[0].legend()
+    axs[0].set_xlabel("Step")
+    axs[0].set_ylabel("Avg. Reward")
+    axs[0].set_title('Epsilon Runs')
+
+
+    # Run Thompson sampling and plot the results
+    thompsonSampling(axs[1], movingBandits=True)
+    axs[1].legend()
+    axs[1].set_xlabel("Step")
+    axs[1].set_ylabel("Avg. Reward")
+    axs[1].set_title("Thompson sampling")
+
+    plt.show()
