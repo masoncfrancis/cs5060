88e2488de6af4da6105ee52881f1f8476c6d6845
Mason <masoncfrancis@gmail.com>
2024-03-15 21:36:05 -0600

    Removed a trailing semicolon

--- hw1/HW1_Francis_Mason.py
+++ hw1/HW1_Francis_Mason.py
@@ -23,54 +23,49 @@ def findStop(numbers: list, outputFileName: str, learnOptimal: bool):
                     solution_found_count[str(i)] += 1
                     if candidate == optimal_candidate:
                         optimal_solution_found_count[str(i)] += 1
-                    
+
                     break
 
     plt.figure(figsize=(30, 6))  # Adjust the size as needed
 
     x, y = zip(*optimal_solution_found_count.items())
 
-    plt.plot(x,y)
+    plt.plot(x, y)
     # plt.show()
     plt.savefig(outputFileName, bbox_inches='tight')
     print("Graph file saved as " + outputFileName)
-    
+
     if learnOptimal:
         maxKey = str(max(optimal_solution_found_count, key=optimal_solution_found_count.get))
         return {maxKey: optimal_solution_found_count[maxKey]}
     else:
         return {'37': optimal_solution_found_count['37']}
-    
+
 
 def problem1(fileName: str):
     print("\nExecuting problem 1 code for " + fileName)
-    s1FileContents = pd.read_csv('scenario1.csv').iloc[:,0].astype(float).tolist()
+    s1FileContents = pd.read_csv('scenario1.csv').iloc[:, 0].astype(float).tolist()
     s1Result37 = findStop(s1FileContents, fileName + '-37.png', False)
     s1ResultOptimalFound = findStop(s1FileContents, fileName + '-optimal.png', True)
     print("Optimal Solution Found Count at 37%: " + str(s1Result37['37']))
     computedPercent = list(s1ResultOptimalFound)[0]
-    print("Optimal Stopping Point found at " + computedPercent +  "%. Count: " + str(s1ResultOptimalFound[computedPercent]))
+    print("Optimal Stopping Point found at " + computedPercent + "%. Count: " + str(
+        s1ResultOptimalFound[computedPercent]))
 
 
 def analyzeDataset(npData):
-    stepsTaken = 0; 
+    stepsTaken = 0
     for i in npData:
         stepsTaken += 1
-        
 
 
 def problem2():
     # I used chatGPT to help me learn how to use NumPy to create datasets of random numbers we can use
-    uniformData = np.random.uniform(1,99, size=1000)
+    uniformData = np.random.uniform(1, 99, size=1000)
     analyzeDataset(uniformData)
 
 
-
-
-
-
 if __name__ == "__main__":
-
     # Problem 1
     print("Executing algorithm for problem 1")
     problem1('scenario1.csv')
@@ -79,4 +74,3 @@ if __name__ == "__main__":
     # Problem 2
     print("Executing algorithm for problem 2")
     problem2()
-
