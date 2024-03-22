2d12761c4488b007f3f16d84d9e29550b8ea6188
Mason <masoncfrancis@gmail.com>
2024-03-22 00:45:54 -0600

    Added a left out plot from q3 part 1 in midterm

--- midterm/q3.py
+++ midterm/q3.py
@@ -31,6 +31,19 @@ def getCloseDict(frame):
     return processedDict
 
 
+def getCloseDiffDict(closeDict: dict):
+    previousClosePrice = 0.0
+    closeDiffDict = dict()
+    for date, closePrice in closeDict.items():
+        if previousClosePrice == 0.0:
+            closeDiffDict[date] = 0.0
+        else:
+            closeDiffDict[date] = closePrice - previousClosePrice
+        previousClosePrice = closePrice
+
+    return closeDiffDict
+
+
 def getHighLowDiffDict(frame):
     processedDict = dict()
     for index, row in frame.iterrows():  # ChatGPT helped me learn how to iterate through a pandas data frame
@@ -50,11 +63,12 @@ def part1():
     print("Running code for Q3 part 1")
 
     # get plotting stuff initialized
-    fix, axs = plt.subplots(2, 2)
+    fix, axs = plt.subplots(2, 3)
 
     # read in AAPL
     appleFrame = readCsv('data/AAPL.csv')
     appleCloseDict = getCloseDict(appleFrame)
+    appleCloseDiffDict = getCloseDiffDict(appleCloseDict)
     appleHighLowDiffDict = getHighLowDiffDict(appleFrame)
 
     # note: for plotting, GitHub copilot helped me format the labels on the x axis
@@ -65,19 +79,29 @@ def part1():
     axs[0, 0].set_ylabel("Value ($)")
     axs[0, 0].set_title("AAPL Closing Values")
     axs[0, 0].set_xticks(
-        [list(appleCloseDict.keys())[0], list(appleCloseDict.keys())[-1]])  # Set xticks to first and last date
+        [list(appleCloseDict.keys())[0], list(appleCloseDict.keys())[-1]])  # Set x axis to only first and last date
+
+    # plot AAPL Close difference values
+    axs[0, 1].plot(appleCloseDiffDict.keys(), appleCloseDiffDict.values())
+    axs[0, 1].set_xlabel("Date")
+    axs[0, 1].set_ylabel("Change in Value ($)")
+    axs[0, 1].set_title("AAPL Closing Difference Values")
+    axs[0, 1].set_xticks(
+        [list(appleCloseDiffDict.keys())[0],
+         list(appleCloseDiffDict.keys())[-1]])  # Set x axis to only first and last date
 
     # plot AAPL High/Low difference values. To calculate, high-low=value was used
-    axs[0, 1].plot(appleHighLowDiffDict.keys(), appleHighLowDiffDict.values())
-    axs[0, 1].set_xlabel("Date")
-    axs[0, 1].set_ylabel("Value ($)")
-    axs[0, 1].set_title("AAPL High/Low Difference")
-    axs[0, 1].set_xticks([list(appleHighLowDiffDict.keys())[0],
-                          list(appleHighLowDiffDict.keys())[-1]])  # Set xticks to first and last date
+    axs[0, 2].plot(appleHighLowDiffDict.keys(), appleHighLowDiffDict.values())
+    axs[0, 2].set_xlabel("Date")
+    axs[0, 2].set_ylabel("Value ($)")
+    axs[0, 2].set_title("AAPL High/Low Difference")
+    axs[0, 2].set_xticks([list(appleHighLowDiffDict.keys())[0],
+                          list(appleHighLowDiffDict.keys())[-1]])  # Set x axis to only first and last date
 
     # read in TSLA
     teslaFrame = readCsv('data/TSLA.csv')
     teslaCloseDict = getCloseDict(teslaFrame)
+    teslaCloseDiffDict = getCloseDiffDict(teslaCloseDict)
     teslaHighLowDiffDict = getHighLowDiffDict(teslaFrame)
 
     # plot TSLA Close values
@@ -86,15 +110,24 @@ def part1():
     axs[1, 0].set_ylabel("Value ($)")
     axs[1, 0].set_title("TSLA Closing Values")
     axs[1, 0].set_xticks(
-        [list(teslaCloseDict.keys())[0], list(teslaCloseDict.keys())[-1]])  # Set xticks to first and last date
+        [list(teslaCloseDict.keys())[0], list(teslaCloseDict.keys())[-1]])  # Set x axis to only first and last date
+
+    # plot AAPL Close difference values
+    axs[1, 1].plot(teslaCloseDiffDict.keys(), teslaCloseDiffDict.values())
+    axs[1, 1].set_xlabel("Date")
+    axs[1, 1].set_ylabel("Change in Value ($)")
+    axs[1, 1].set_title("TSLA Closing Difference Values")
+    axs[1, 1].set_xticks(
+        [list(teslaCloseDiffDict.keys())[0],
+         list(teslaCloseDiffDict.keys())[-1]])  # Set x axis to only first and last date
 
     # plot TSLA High/Low difference values. To calculate, high-low=value was used
-    axs[1, 1].plot(teslaHighLowDiffDict.keys(), teslaHighLowDiffDict.values())
-    axs[1, 1].set_xlabel("Date")
-    axs[1, 1].set_ylabel("Value ($)")
-    axs[1, 1].set_title("TSLA High/Low Difference")
-    axs[1, 1].set_xticks([list(teslaHighLowDiffDict.keys())[0],
-                          list(teslaHighLowDiffDict.keys())[-1]])  # Set xticks to first and last date
+    axs[1, 2].plot(teslaHighLowDiffDict.keys(), teslaHighLowDiffDict.values())
+    axs[1, 2].set_xlabel("Date")
+    axs[1, 2].set_ylabel("Value ($)")
+    axs[1, 2].set_title("TSLA High/Low Difference")
+    axs[1, 2].set_xticks([list(teslaHighLowDiffDict.keys())[0],
+                          list(teslaHighLowDiffDict.keys())[-1]])  # Set x axis to only first and last date
 
     plt.show()
 
